cbc23a8a91e1b76c66beff4a1355f24d63e5b061
Kilouhardi <Joshual.courriel@gmail.com>
2022-06-06 21:16:00 +0200

    bye les sauvegardes bye

--- Rendu phase 1/LoopHero/src/theGame/boardGame/Coord.java
+++
@@ -1,4 +0,0 @@
-package theGame.boardGame;
-
-public record Coord(int x,int y) {}
-//simple stockage de coordonees
--- Rendu phase 3/LoopHero/src/theGame/boardGame/Coord.java
+++
@@ -1,6 +0,0 @@
-package theGame.boardGame;
-
-import java.io.Serializable;
-
-public record Coord(int x,int y) implements Serializable {}
-//Simple coord stockage
